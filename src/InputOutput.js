7cf3e38a915e7013ef39116cc079c21a8a1bee78
Marcin <marcin.p.dev@gmail.com>
2022-09-26 16:53:47 +0200

    Markdown function implemented for testing

--- src/InputOutput.js
+++ src/InputOutput.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import './InputOutput.css';
+import React from "react";
+import "./InputOutput.css";
+// import Markdown from "marked-react";
+import { marked } from "marked";
 
 class InputOutput extends React.Component {
-    constructor (props){
-        super(props)
-    this.state = {text: ''}
-    
-    }
-render() {
+  constructor(props) {
+    super(props);
+    this.state = { text: "" };
+  }
+
+  getMarkdownText() {
+    var rawMarkup = marked.parse("# header This is _Markdown_.");
+    return { __html: rawMarkup };
+  }
+
+  render() {
     return (
-        <React.Fragment>
-            test
-
-
-        </React.Fragment>
-    )
+      <React.Fragment>
+        <textarea id="editor"></textarea>
+        <div id="preview">
+          <div dangerouslySetInnerHTML={this.getMarkdownText()} />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
-
-}
-
+//marked.parse('# Marked in the browser\n\nRendered by **marked**.');
 
 export default InputOutput;
