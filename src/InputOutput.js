adf3c75276746d541d212d33097b63286b178d48
Marcin <marcin.p.dev@gmail.com>
2022-09-26 16:09:39 +0200

    Inputoutput module created

---
+++ src/InputOutput.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './InputOutput.css';
+
+class InputOutput extends React.Component {
+    constructor (props){
+        super(props)
+    this.state = {text: ''}
+    
+    }
+
+
+
+}
+
+
+export default InputOutput;
